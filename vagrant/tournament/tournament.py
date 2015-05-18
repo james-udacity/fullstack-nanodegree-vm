41c5cf499dcb00fe3709a0bcce086c81dec42f2f
James Williams <James.L.Williams@gmail.com>
2015-05-18 08:47:41 -0700

    Stuff.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,7 +4,7 @@
 #
 
 import psycopg2
-
+from itertools import izip
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -13,21 +13,31 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    pass
+    db = connect()
+    cur = db.cursor()
+    cur.execute("DELETE FROM standings")
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
     db = connect()
     cur = db.cursor()
+    # If you are deleting players, you need to remove them
+    # from the standings table as well so there aren't any
+    # orphan standings for players that no longer exist.
     cur.execute("DELETE FROM standings")
     cur.execute("DELETE FROM players")
     db.commit()
+    db.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    cur = connect().cursor()
+    db = connect()
+    cur = db.cursor()
     cur.execute("SELECT count(*) from players")
     count = cur.fetchall()
+    db.close()
     return count[0][0]
 
 def registerPlayer(name):
@@ -43,9 +53,10 @@ def registerPlayer(name):
     cur = db.cursor()
     cur.execute("INSERT into players VALUES (%s)", (name,))
     cur.execute("SELECT lastval()")
-    id = cur.fetchall()[0]
-    cur.execute("INSERT into standings VALUES (%s)", (id,))
+    player_id = cur.fetchall()[0]
+    cur.execute("INSERT into standings VALUES (%s)", (player_id,))
     db.commit()
+    db.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -63,7 +74,8 @@ def playerStandings():
     db = connect()
     cur = db.cursor()
     cur.execute("SELECT id, name, wins, losses from standings, players where player_id = id")
-    standings = [(str(row[0]), str(row[1]),row[2], (row[2]+row[3])) for row in cur.fetchall()]
+    standings = [(str(row[0]), str(row[1]), row[2], (row[2]+row[3])) for row in cur.fetchall()]
+    db.close()
     return standings
 
 def reportMatch(winner, loser):
@@ -78,6 +90,7 @@ def reportMatch(winner, loser):
     cur.execute("UPDATE standings SET wins = wins + 1 WHERE player_id = %s ", (winner,))
     cur.execute("UPDATE standings SET losses = losses + 1 WHERE player_id = %s ", (loser,))
     db.commit()
+    db.close()
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
@@ -94,5 +107,21 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    matches = []
+    db = connect()
+    cur = db.cursor()
+    cur.execute("SELECT player_id, name FROM standings, players where id = player_id ORDER BY wins")
+    players = cur.fetchall()
+    db.close()
+    for player1, player2 in pairwise(players):
+        matches.append((str(player1[0]), player1[1], str(player2[0]), player2[1]))
+    return matches
+
+# From http://stackoverflow.com/a/5389547
+def pairwise(iterable):
+    """Allows iterating throw the iterable by twos, makes the swiss pairings much easier"""
+    item = iter(iterable)
+    return izip(item, item)
+
 
 
